9a205df3a30def2646323903cb2640d5ad7cb998
cakesmack <cmack6189@gmail.com>
2023-02-15 23:17:26 +0000

    Created email, api files. added subject line.

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
 import requests
+from send_email import send_email
 
-api_key = '0eab0885700542848bfb89d93c279600'
-url = 'https://newsapi.org/v2/everything?q=tesla&from=2023-01-15&sortBy=publishedAt&apiKey=0eab0885700542848bfb89d93c279600'
+# Import api key from text file to keep private. Replace {api} with API key
+api_file = open('api.txt')
+api = api_file.read()
+api_key = api
+
+topic = 'guitar'
+
+url = 'https://newsapi.org/v2/everything?'\
+    f'q={topic}'\
+    '&from=2023-01-15'\
+    '&sortBy=publishedAt'\
+    f'&apiKey={api}'\
+    '&language=en'
 
 r = requests.get(url)
+
 page = r.json()
 
 content = page['articles']
 
 
-for i in content:
-    print(i['title'])
+body = f'Subject: Todays {topic} News'
+
+for i in content[:20]:
+    title = i['title']
+    author = i['author']
+    date = i['publishedAt']
+    link = i['url']
+    body = body + f'''
+
+    {title}
+
+    Published By - {author} on {date}
+    {link}
+    *****
+
+    '''
+body = body.encode('utf-8')
+send_email(message=body)
+
 
 
 
